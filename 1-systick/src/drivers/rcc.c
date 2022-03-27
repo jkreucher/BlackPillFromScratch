1e8636d5c3ed9d78c6ff6657d91136925b6ba6bc
jkreucher <linusmueller44@gmx.de>
2022-03-27 03:01:50 +0200

    systick implementation

---
+++ 1-systick/src/drivers/rcc.c
@@ -0,0 +1,11 @@
+#include "rcc.h"
+
+void rcc_init() {
+	// puts rcc struct to the right place
+	// "volatile" tells the compiler that rcc can be changed by hardware
+	volatile RCC_t* rcc = (RCC_t*)ADDR_BASE_RCC;
+	// enable all gpio ports
+	rcc->ahb1enr |= RCC_EN_GPIOA | RCC_EN_GPIOB | RCC_EN_GPIOC;
+	// enable power control
+	rcc->ahb1enr |= RCC_EN_POWER;
+}
---
+++ 1-systick/src/drivers/stm32f4.h
@@ -0,0 +1,11 @@
+#ifndef STM32F4_H_
+#define STM32F4_H_
+
+// Register addresses
+#define ADDR_BASE_TICK	0xE000E010U
+#define ADDR_BASE_RCC	0x40023800U
+#define ADDR_BASE_GPIOC	0x40020800U
+#define ADDR_BASE_GPIOB	0x40020400U
+#define ADDR_BASE_GPIOA	0x40020000U
+
+#endif
